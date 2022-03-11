85e3aeb4a2ca755ba03b119c0b93af811a976303
Brian Luong <bhluong1codes@gmail.com>
2022-03-10 22:20:43 -0800

    User can add a note

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -1,29 +1,18 @@
-// const fs = require('fs');
-const { notes } = require('./data.json');
+const fs = require('fs');
+const dataJson = require('./data.json');
+const { nextId } = require('./data.json');
 
 if (process.argv[2] === 'read') {
-  for (const property in notes) {
-    console.log(`${property}: ${notes[property]}`);
+  for (const property in dataJson.notes) {
+    console.log(`${property}: ${dataJson.notes[property]}`);
   }
+} else if (process.argv[2] === 'create' && process.argv[3]) {
+  dataJson.notes[nextId] = process.argv[3];
+  dataJson.nextId++;
 }
 
-// fs.readFile('data.json', 'utf8', (err, data) => {
-//   if (err) {
-//     console.log(err);
-//     return;
-//   }
-
-//   var { notes } = data;
-//   console.log(notes);
-//   console.log(typeof notes);
-// });
-
-// if (process.argv[3] === 'plus') {
-//   console.log('result:', add(firstNum, secondNum));
-// } else if (process.argv[3] === 'minus') {
-//   console.log('result:', subtract(firstNum, secondNum));
-// } else if (process.argv[3] === 'times') {
-//   console.log('result:', multiply(firstNum, secondNum));
-// } else if (process.argv[3] === 'over') {
-//   console.log('result:', divide(firstNum, secondNum));
-// }
+fs.writeFile('data.json', JSON.stringify(dataJson, null, 2), err => {
+  if (err) {
+    console.error(err);
+  }
+});
