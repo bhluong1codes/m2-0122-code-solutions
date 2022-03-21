6ff3ec987f4ec43c02a2241884ba937831f76772
Brian Luong <bhluong1codes@gmail.com>
2022-03-20 22:24:01 -0700

    User can get a single note by id

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const app = express();
 
-const notes = {};
+const notes =
+  {
+    1:
+    {
+      content: 'The event loop is how a JavaScript runtime pushes asynchronous callbacks onto the stack once the stack is cleared.',
+      id: 1
+    },
+    2:
+    {
+      content: 'Prototypal inheritance is how JavaScript objects delegate behavior.',
+      id: 2
+    }
+
+  };
 
 app.get('/api/notes', (req, res) => {
   const notesArr = [];
@@ -9,6 +22,24 @@ app.get('/api/notes', (req, res) => {
     notesArr.push(notes[properties]);
   }
   res.send(notesArr);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id);
+  if (id < 1 || isNaN(id)) {
+    const err = {
+      error: 'id must be a positive integer'
+    };
+    res.status(400).json(err);
+    res.send(typeof id);
+  } else if (!notes[id]) {
+    const notFound = {
+      error: `cannot find note with id ${id}`
+    };
+    res.status(404).json(notFound);
+  } else {
+    res.json(notes[id]);
+  }
 });
 
 app.listen(3000, () =>
