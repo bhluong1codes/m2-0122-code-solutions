72487ee0452b1ace22ee9ad368ad88561418c031
Brian Luong <bhluong1codes@gmail.com>
2022-03-22 21:20:25 -0700

    index.js now writes back to the data.json file

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
 
-const notes = {};
-let nextId = 1;
+const fs = require('fs');
+const dataJson = require('./data.json');
 
 app.get('/api/notes', (req, res) => {
   const notesArr = [];
-  for (const properties in notes) {
-    notesArr.push(notes[properties]);
+  for (const properties in dataJson.notes) {
+    notesArr.push(dataJson.notes[properties]);
   }
   res.send(notesArr);
 });
@@ -15,18 +15,17 @@ app.get('/api/notes', (req, res) => {
 app.get('/api/notes/:id', (req, res) => {
   const id = Number(req.params.id);
   if (id < 1 || isNaN(id)) {
-    const err = {
+    const errMsg = {
       error: 'id must be a positive integer'
     };
-    res.status(400).json(err);
-    res.send(typeof id);
-  } else if (!notes[id]) {
+    res.status(400).json(errMsg);
+  } else if (!dataJson.notes[id]) {
     const notFound = {
       error: `cannot find note with id ${id}`
     };
     res.status(404).json(notFound);
   } else {
-    res.json(notes[id]);
+    res.json(dataJson.notes[id]);
   }
 });
 
@@ -34,8 +33,7 @@ const jsonMiddleware = express.json();
 
 app.use(jsonMiddleware);
 
-app.post('/api/notes', (req, res, err) => {
-
+app.post('/api/notes', (req, res) => {
   const newNote = req.body;
   if (!newNote.content) {
     const errMsg = {
@@ -43,15 +41,25 @@ app.post('/api/notes', (req, res, err) => {
     };
     res.status(400).send(errMsg);
   } else {
-    newNote.id = nextId;
-    notes[nextId] = newNote;
-    nextId++;
+    const id = dataJson.nextId++;
+    newNote.id = id;
+    dataJson.notes[id] = newNote;
     res.status(201).send(newNote);
   }
-
+  writeFile();
 });
 
-app.listen(3000, () =>
+app.listen(3000, () => {
 // eslint-disable-next-line no-console
-  console.log('Listening to port 3000')
+  console.log('Listening to port 3000');
+}
 );
+
+const writeFile = () => {
+  const dataStringify = JSON.stringify(dataJson, null, 2);
+  fs.writeFile('data.json', dataStringify, err => {
+    if (err) {
+      console.error(err);
+    }
+  });
+};
