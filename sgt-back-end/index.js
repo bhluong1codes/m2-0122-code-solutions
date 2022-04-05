b2eca123423786569327a7d69fd60c7cf817f0c5
Brian Luong <bhluong1codes@gmail.com>
2022-04-04 21:55:01 -0700

    Created index.js file

---
+++ sgt-back-end/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('Express server listening on port 3000');
+});
+
+const pg = require('pg');
+
+// only create ONE pool for your whole server
+const db = new pg.Pool({
+  connectionString: 'postgres://dev:dev@localhost/studentGradeTable',
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+db.query();
