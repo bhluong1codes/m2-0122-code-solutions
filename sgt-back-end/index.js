2424c924d9dc50ccbcf2b7f69a7ca3fb4a2a314b
Brian Luong <bhluong1codes@gmail.com>
2022-04-06 22:31:01 -0700

    User can add a grade

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -22,13 +22,61 @@ app.use(jsonMiddleware);
 
 app.get('/api/grades', (req, res) => {
   const sql = `
-  SELECT *
+  select *
     from "grades"
   `;
   db.query(sql)
     .then(result => {
       const grades = result.rows;
       res.status(200).json(grades);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'An unexpected error occurred' });
+    });
+});
+
+app.post('/api/grades', (req, res) => {
+
+  const values = [];
+  const name = req.body.name;
+  const course = req.body.course;
+  const score = Number(req.body.score);
+  values.push(name);
+  values.push(course);
+  values.push(score);
+
+  if (!name && !course) {
+    res.status(400).json({
+      error: 'name & course are required'
+    });
+    return;
+  } else if (!course) {
+    res.status(400).json({
+      error: 'course is required'
+    });
+    return;
+  } else if (!name) {
+    res.status(400).json({
+      error: 'name is required'
+    });
+    return;
+  } else if (!Number.isInteger(score) || score < 0) {
+    res.status(400).json({
+      error: 'score must be a positive integer'
+    });
+    return;
+  }
+
+  const sql = `
+    insert into "grades" ("name", "course", "score")
+      values ($1, $2, $3)
+      returning *;
+  `;
+
+  db.query(sql, values)
+    .then(result => {
+      res.status(201).json(result.rows);
     })
     .catch(err => {
       console.error(err);
